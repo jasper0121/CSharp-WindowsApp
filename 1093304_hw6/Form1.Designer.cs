40f2f1f34374d76366355349ebcab29306e77943
Jasper <z36760376@gmail.com>
2025-03-05 00:05:13 +0800

    HW6

---
+++ 1093304_hw6/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace _1093304_hw6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.gameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.restartToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gameToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(612, 30);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // gameToolStripMenuItem
+            // 
+            this.gameToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.restartToolStripMenuItem});
+            this.gameToolStripMenuItem.Name = "gameToolStripMenuItem";
+            this.gameToolStripMenuItem.Size = new System.Drawing.Size(64, 23);
+            this.gameToolStripMenuItem.Text = "Game";
+            // 
+            // restartToolStripMenuItem
+            // 
+            this.restartToolStripMenuItem.Name = "restartToolStripMenuItem";
+            this.restartToolStripMenuItem.Size = new System.Drawing.Size(141, 26);
+            this.restartToolStripMenuItem.Text = "Restart";
+            this.restartToolStripMenuItem.Click += new System.EventHandler(this.restartToolStripMenuItem_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Enabled = true;
+            this.timer3.Interval = 1000;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 9F);
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(12, 541);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Remaining:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(91, 541);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 17);
+            this.label2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(12, 576);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Received:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(91, 576);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 17);
+            this.label4.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 604);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem gameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem restartToolStripMenuItem;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ 1093304_hw6/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace _1093304_hw6
+{
+    public partial class Form1 : Form
+    {
+        Image[] bg = new Image[3] { Properties.Resources.Hydrangeas, Properties.Resources.Penguins, Properties.Resources.Tulips }; //背景圖
+        Image[] fruit = new Image[3] { Properties.Resources.Banana, Properties.Resources.StawBerry, Properties.Resources.Tomato }; //3種水果
+        Image bowl = Properties.Resources.Bowl; //碗       
+        Rectangle rectDest; //設定視窗上的矩形範圍
+        Point[] startPos = new Point[3]; //3種水果開始掉落的點座標
+        Point bgSize, bowlPos; //設定背景長寬、碗的正中心點座標
+        Random rd = new Random(); //亂數
+        int cur = 0, counter, second; //當前的背景圖、碗中水果數、秒數
+        bool playing; //遊玩狀態
+        float[][] cmArray = //色彩調整矩陣，調整為半透明
+           { new float[] { 1, 0, 0, 0, 0 }, new float[] { 0, 1, 0, 0, 0 }, new float[] { 0, 0, 1, 0, 0 }, new float[] { 0, 0, 0, 0.5f, 0 }, new float[] { 0, 0, 0, 0, 1 } };
+
+        public Form1()
+        {
+            InitializeComponent();
+            bgSize = new Point(bg[0].Width / 2, 350); //背景固定長寬           
+            rectDest = new Rectangle(0, 25, bgSize.X, bgSize.Y); //設定視窗上的矩形固定範圍
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Size = new Size(bgSize.X + 16, 550); //設定視窗大小
+            for (int i = 0; i < 3; startPos[i++] = new Point(rd.Next(10, bgSize.X - 40), rd.Next(0, 20))) ; //亂數給定3種水果的起始掉落座標
+            label2.Text = (second = 120).ToString() + " Seconds"; //初始遊戲時間倒數120秒
+            label4.Text = (counter = 0).ToString(); //初始碗中水果數為0           
+            bowlPos = new Point(bgSize.X / 2, bgSize.Y + 25); //碗的正中心
+            playing = true; //true == 開始，false == 結束
+            timer1.Start(); //每10秒換一次背景
+            timer2.Start(); //每1毫秒刷新水果位置
+            timer3.Start(); //倒數計時120秒
+        }
+
+        private void restartToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Invalidate(); //介面失效
+            timer1.Stop(); timer2.Stop(); timer3.Stop(); //所有計時暫停
+            Form1_Load(sender, e); //所有參數初始化
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            ColorMatrix cm = new ColorMatrix(cmArray); //放入色彩調整矩陣
+            ImageAttributes ia = new ImageAttributes();
+            ia.SetColorMatrix(cm, ColorMatrixFlag.Default, ColorAdjustType.Bitmap); //將圖片調整為半透明
+            e.Graphics.DrawImage(bg[cur], rectDest, 0, 0, bg[cur].Width, bg[cur].Height, GraphicsUnit.Pixel, ia); //繪出半透明背景
+            e.Graphics.DrawImage(bowl, bowlPos.X - bowl.Width / 2, bowlPos.Y, bowl.Width, bowl.Height); //繪出碗
+            for (int i = 0; i < 3; i++) //繪出3種水果
+            {
+                e.Graphics.DrawImage(fruit[i], startPos[i].X, startPos[i].Y, fruit[i].Width, fruit[i].Height);
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (playing) //若正在遊戲中
+            {
+                //設定碗可移動的左右邊界，一旦滑鼠移動，則更改碗的中心位置
+                bowlPos.X = e.X < bowl.Width / 2 ? bowl.Width / 2 : (e.X > bgSize.X - bowl.Width / 2 ? bgSize.X - bowl.Width / 2 : e.X);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) //每10秒換一次背景
+        {
+            ++cur; cur %= 3; //cur == 0 ~ 2循環(對應bg[0] ~ bg[2]中的3張背景)
+            Invalidate();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e) //每1毫秒刷新水果位置
+        {
+            for (int i = 0; i < 3; ++startPos[i++].Y) //每次將該水果Y座標 + 1
+            {
+                if (startPos[i].Y == bgSize.Y) //若水果到達底部
+                {
+                    //若水果的長度中心點在碗的範圍內
+                    if (bowlPos.X - bowl.Width / 2 <= startPos[i].X + fruit[i].Width / 2 && startPos[i].X + fruit[i].Width / 2 <= bowlPos.X + bowl.Width / 2)
+                    {
+                        label4.Text = (++counter).ToString(); //則視為該水果有被碗接住，碗內水果數 + 1
+                    }
+
+                    startPos[i] = new Point(rd.Next(10, bgSize.X - 40), rd.Next(0, 20)); //重新設定起始掉落座標
+                }
+            }
+
+            Invalidate();
+        }
+
+        private void timer3_Tick(object sender, EventArgs e) //倒數計時120秒
+        {
+            if (playing) //若正在遊戲中
+            {
+                label2.Text = (--second).ToString() + " Seconds";
+                if (second == 0) //若倒數計時至0，則所有計時停止，遊玩狀態結束
+                {
+                    timer1.Stop(); timer2.Stop(); timer3.Stop(); //所有計時暫停
+                    playing = false; //遊戲結束
+                }
+            }
+        }
+    }
+}
